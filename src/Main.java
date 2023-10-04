d0a7ff1e673a66bb8eca0e8226e00abdf118bd00
Alina Tibulca <tibulkalina@mail.ru>
2023-10-04 23:11:47 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+
+    public static void main(String[] args) {
+        CreditPaymentService paymentService = new CreditPaymentService();
+
+        double loanAmount = 1000000; // Cумма кредита в рублях
+        double interestRate = 9.99; // Годовая процентная ставка
+        int loanTerm1 = 1;  // Срок кредита в годах
+        int loanTerm2 = 2;
+        int loanTerm3 = 3;
+
+        double monthlyPayment1 = paymentService.calculatePayment(loanAmount, interestRate, loanTerm1);
+        double monthlyPayment2 = paymentService.calculatePayment(loanAmount, interestRate, loanTerm2);
+        double monthlyPayment3 = paymentService.calculatePayment(loanAmount, interestRate, loanTerm3);
+
+        System.out.println("Ежемесячный платеж на 1 год: " + monthlyPayment1 + " рублей");
+        System.out.println("Ежемесячный платеж на 2 года: " + monthlyPayment2 + " рублей");
+        System.out.println("Ежемесячный платеж на 3 года: " + monthlyPayment3 + " рублей");
+    }
+}
